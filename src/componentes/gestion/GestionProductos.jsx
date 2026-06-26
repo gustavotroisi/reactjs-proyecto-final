1a4f31a6a0ef4acefe30c980ffb35885cf1d7559
Gustavo Troisi <gustavotroisi@gmail.com>
2026-06-26 01:12:49 -0300

    fix error th GestionProductos

--- src/componentes/gestion/GestionProductos.jsx
+++ src/componentes/gestion/GestionProductos.jsx
@@ -50,35 +50,41 @@ const GestionProductos = () => {
           <h3 className={styles.subtitulo}>Listado de Productos</h3>
           <table className={styles.gestionTable}>
             <thead>
-              <th>ID</th>
-              <th>Nombre</th>
-              <th>Precio</th>
-              <th>Acciones</th>
-            </thead>
-            {productos.map((prod) => (
-              <tr key={prod.id}>
-                <td style={{ textAlign: "right" }}>{prod.id}</td>
-                <td>{prod.nombre}</td>
-                <td style={{ textAlign: "right" }}>$ {prod.precio}</td>
-                <td>
-                  <Button
-                    onClick={() => alert(`Editar producto con ID: ${prod.id}`)}
-                    variant="primary"
-                    className="my-2"
-                    style={{ marginRight: "10px" }}
-                  >
-                    <FaEdit style={{ marginRight: "5px" }} /> Editar
-                  </Button>
-                  <Button
-                    onClick={() => handleDelete(prod.id)}
-                    variant="danger"
-                    className="my-2"
-                  >
-                    <FaTrash style={{ marginRight: "5px" }} /> Eliminar
-                  </Button>
-                </td>
+              <tr>
+                <th>ID</th>
+                <th>Nombre</th>
+                <th>Precio</th>
+                <th>Acciones</th>
               </tr>
-            ))}
+            </thead>
+            <tbody>
+              {productos.map((prod) => (
+                <tr key={prod.id}>
+                  <td style={{ textAlign: "right" }}>{prod.id}</td>
+                  <td>{prod.nombre}</td>
+                  <td style={{ textAlign: "right" }}>$ {prod.precio}</td>
+                  <td>
+                    <Button
+                      onClick={() =>
+                        alert(`Editar producto con ID: ${prod.id}`)
+                      }
+                      variant="primary"
+                      className="my-2"
+                      style={{ marginRight: "10px" }}
+                    >
+                      <FaEdit style={{ marginRight: "5px" }} /> Editar
+                    </Button>
+                    <Button
+                      onClick={() => handleDelete(prod.id)}
+                      variant="danger"
+                      className="my-2"
+                    >
+                      <FaTrash style={{ marginRight: "5px" }} /> Eliminar
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
           </table>
         </Col>
       </Row>
